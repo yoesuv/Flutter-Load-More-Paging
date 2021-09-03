0526f5933f05f6fd33e490b40ae842d215e63e74
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2021-09-03 10:50:37 +0700

    :newspaper: add networking file

---
+++ lib/src/core/networks/logging_interceptor.dart
@@ -0,0 +1,20 @@
+import 'package:dio/dio.dart';
+
+class LoggingInterceptor extends Interceptor{
+
+  @override
+  void onResponse(Response response, ResponseInterceptorHandler handler) {
+    super.onResponse(response, handler);
+    print('\n\n');
+    print('<--- HTTP CODE : ${response.statusCode} URL : ${response.realUri.toString()}');
+    print('Headers: ');
+    printWrapped('Response : ${response.data}');
+    print('<--- END HTTP');
+  }
+
+  void printWrapped(String text) {
+    final RegExp pattern = RegExp('.{1,800}');
+    pattern.allMatches(text).forEach((RegExpMatch match) => print(match.group(0)));
+  }
+
+}
