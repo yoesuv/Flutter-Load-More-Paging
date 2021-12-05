a0e92a805d653c4dc08aa319ee1a44211d23baa1
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2021-12-05 15:50:13 +0700

    :pencil: update bloc paging

--- lib/src/core/blocs/home_list_bloc.dart
+++ lib/src/core/blocs/home_list_bloc.dart
@@ -1,41 +1,41 @@
 import 'package:bloc/bloc.dart';
+import 'package:bloc_concurrency/bloc_concurrency.dart';
 import 'package:bloc_load_more/src/core/events/home_list_event.dart';
 import 'package:bloc_load_more/src/core/repositories/app_repository.dart';
 import 'package:bloc_load_more/src/core/states/home_list_state.dart';
-import 'package:rxdart/rxdart.dart';
+import 'package:stream_transform/stream_transform.dart';
+
+const throttleDuration = Duration(milliseconds: 100);
+
+EventTransformer<E> throttleDroppable<E>(Duration duration) {
+  return (events, mapper) {
+    return droppable<E>().call(events.throttle(duration), mapper);
+  };
+}
 
 class HomeListBloc extends Bloc<HomeListEvent, HomeListState> {
 
-  HomeListBloc() : super(const HomeListState());
-
-  @override
-  Stream<Transition<HomeListEvent, HomeListState>> transformEvents(
-      Stream<HomeListEvent> events, TransitionFunction<HomeListEvent, HomeListState> transitionFn) {
-    return super.transformEvents(events.throttleTime(const Duration(milliseconds: 500)), transitionFn);
+  HomeListBloc() : super(const HomeListState()) {
+    on<HomeListEventLoad>(_onPostFetched, transformer: throttleDroppable(throttleDuration));
   }
 
-  @override
-  Stream<HomeListState> mapEventToState(HomeListEvent event) async* {
-    if (event is HomeListEventLoad) {
-      yield await _mapPostFetchedToState(state);
-    }
-  }
-
-  Future<HomeListState> _mapPostFetchedToState(HomeListState state) async {
-    if (state.hasReachedMax) return state;
+  Future<void> _onPostFetched(HomeListEvent event, Emitter<HomeListState> emit) async{
+    if (state.hasReachedMax) return;
     try {
       if (state.status == PostStatus.initial) {
         final posts = await AppRepository().getListPost(0);
-        return state.copyWith(status: PostStatus.success, posts: posts, hasReachedMax: false);
+        return emit(state.copyWith(
+            status: PostStatus.success, posts: posts, hasReachedMax: false
+        ));
       }
       final posts = await AppRepository().getListPost(state.posts.length);
       if (posts.isEmpty) {
-        return state.copyWith(hasReachedMax: true);
+        emit(state.copyWith(hasReachedMax: true));
       } else {
-        return state.copyWith(status: PostStatus.success, posts: List.of(state.posts)..addAll(posts), hasReachedMax: false);
+        emit(state.copyWith(status: PostStatus.success, posts: List.of(state.posts)..addAll(posts), hasReachedMax: false));
       }
     } catch (e) {
-      return state.copyWith(status: PostStatus.failure);
+      emit(state.copyWith(status: PostStatus.failure));
     }
   }
 
