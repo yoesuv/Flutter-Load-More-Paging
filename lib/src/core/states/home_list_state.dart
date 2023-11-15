69a88a5ea768d47c1ec433157d1244608ce93e89
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-11-15 20:51:22 +0700

    :pencil: update code

--- lib/src/core/states/home_list_state.dart
+++ lib/src/core/states/home_list_state.dart
@@ -1,31 +1,35 @@
 import 'package:bloc_load_more/src/core/models/post_response.dart';
 import 'package:equatable/equatable.dart';
 
-enum PostStatus {
-  initial, success, failure
-}
+enum PostStatus { initial, success, failure }
 
 class HomeListState extends Equatable {
-
   const HomeListState({
     this.status = PostStatus.initial,
     this.posts = const <Post>[],
-    this.hasReachedMax = false
+    this.hasReachedMax = false,
   });
 
   final PostStatus status;
   final List<Post> posts;
   final bool hasReachedMax;
 
-  HomeListState copyWith({PostStatus? status, List<Post>? posts, bool? hasReachedMax}) {
+  HomeListState copyWith({
+    PostStatus? status,
+    List<Post>? posts,
+    bool? hasReachedMax,
+  }) {
     return HomeListState(
       status: status ?? this.status,
       posts: posts ?? this.posts,
-      hasReachedMax:  hasReachedMax ?? this.hasReachedMax
+      hasReachedMax: hasReachedMax ?? this.hasReachedMax,
     );
   }
 
   @override
-  List<Object?> get props => [status, posts, hasReachedMax];
-
+  List<Object?> get props => [
+        status,
+        posts,
+        hasReachedMax,
+      ];
 }
