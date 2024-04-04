8853db74f0ba2109c86e27473a6415a30fa93249
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2024-04-05 04:28:49 +0700

    :wrench: apply linter

--- lib/src/my_app.dart
+++ lib/src/my_app.dart
@@ -2,12 +2,17 @@ import 'package:bloc_load_more/src/core/routes/app_route.dart';
 import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter BLOC Load More',
       theme: ThemeData(
-        primarySwatch: Colors.teal,
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal).copyWith(
+          background: Colors.white,
+        ),
       ),
       onGenerateRoute: AppRoute.routes,
     );
--- lib/src/ui/start.dart
+++ lib/src/ui/start.dart
@@ -4,30 +4,45 @@ import 'package:bloc_load_more/src/ui/widgets/button_primary.dart';
 import 'package:flutter/material.dart';
 
 class Start extends StatelessWidget {
-
   static const String routeName = 'start';
 
-  VoidCallback goToList(BuildContext context) => (){
-    Navigator.pushNamed(context, Home.routeName);
-  };
+  const Start({super.key});
 
-  VoidCallback goToGrid(BuildContext context) => (){
-    Navigator.pushNamed(context, Grid.routeName);
-  };
+  VoidCallback goToList(BuildContext context) => () {
+        Navigator.pushNamed(
+          context,
+          Home.routeName,
+        );
+      };
+
+  VoidCallback goToGrid(BuildContext context) => () {
+        Navigator.pushNamed(
+          context,
+          Grid.routeName,
+        );
+      };
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Flutter Infinite List')),
+      appBar: AppBar(
+        title: const Text('Flutter Infinite List'),
+      ),
       body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 20),
+        padding: const EdgeInsets.symmetric(horizontal: 20),
         child: Center(
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
-              ButtonPrimary(onPress: goToList(context), text: 'Pagination List'),
-              SizedBox(height: 20),
-              ButtonPrimary( onPress: goToGrid(context), text: 'Pagination Grid'),
+              ButtonPrimary(
+                onPress: goToList(context),
+                text: 'Pagination List',
+              ),
+              const SizedBox(height: 20),
+              ButtonPrimary(
+                onPress: goToGrid(context),
+                text: 'Pagination Grid',
+              ),
             ],
           ),
         ),
