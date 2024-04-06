81dc739de5eaec7ad6c0ae482dd54b5c3fecee2f
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2024-04-07 05:24:48 +0700

    :hammer: refactor app route

--- lib/src/core/routes/app_route.dart
+++ lib/src/core/routes/app_route.dart
@@ -1,3 +1,4 @@
+import 'package:bloc_load_more/src/ui/expanded_list.dart';
 import 'package:bloc_load_more/src/ui/grid.dart';
 import 'package:bloc_load_more/src/ui/home.dart';
 import 'package:bloc_load_more/src/ui/splash.dart';
@@ -6,37 +7,42 @@ import 'package:flutter/material.dart';
 
 class AppRoute {
   static Route<dynamic> routes(RouteSettings settings) {
-    if (settings.name == '/') {
-      return MaterialPageRoute(builder: (context) {
-        return const Splash();
-      });
-    } else if (settings.name == Home.routeName) {
-      return MaterialPageRoute(builder: (context) {
-        return const Home();
-      });
-    } else if (settings.name == Start.routeName) {
-      return MaterialPageRoute(builder: (context) {
-        return const Start();
-      });
-    } else if (settings.name == Grid.routeName) {
-      return MaterialPageRoute(builder: (context) {
-        return const Grid();
-      });
-    } else {
-      return MaterialPageRoute(builder: (context) {
-        return const Scaffold(
-          body: Center(
-            child: Text(
-              'Page Not Found',
-              style: TextStyle(
-                color: Colors.red,
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
+    switch (settings.name) {
+      case "/":
+        return MaterialPageRoute(builder: (context) {
+          return const Splash();
+        });
+      case Home.routeName:
+        return MaterialPageRoute(builder: (context) {
+          return const Home();
+        });
+      case Start.routeName:
+        return MaterialPageRoute(builder: (context) {
+          return const Start();
+        });
+      case Grid.routeName:
+        return MaterialPageRoute(builder: (context) {
+          return const Grid();
+        });
+      case ExpandedList.routeName:
+        return MaterialPageRoute(builder: (context) {
+          return const ExpandedList();
+        });
+      default:
+        return MaterialPageRoute(builder: (context) {
+          return const Scaffold(
+            body: Center(
+              child: Text(
+                'Page Not Found',
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
-          ),
-        );
-      });
+          );
+        });
     }
   }
 }
